0d48ef94d40e2d0596577658a8fc8fdca41d81fd
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2023-06-21 17:39:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from pyrogram import Client
-from ToXic.api import start, downloader, helper
-from ToXic.api.users import is_served_user, get_served_users, add_served_user
-from ToXic.api.broadcast import stats, broadcast
 
 api_id = 
 api_hash = ""
@@ -12,7 +9,8 @@ app = Client(
     api_id=api_id,
     api_hash=api_hash,
     bot_token=bot_token,
-    plugins={"root": "ToXic.api", "users": "ToXic.users", "broadcast": "ToXic.broadcast"},
+    in_memory=True,
+    plugins={"root": "ToXic"},
 )
 
 app.run()
