ecb16d44069ba0754cd249c6af7f02f14a57814a
٭Bιĸαѕн٭ <127423650+Bikash1225@users.noreply.github.com>
2023-06-21 17:17:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from ToXic.api import start, downloader, helper
 from ToXic.api.users import is_served_user, get_served_users, add_served_user
 from ToXic.api.broadcast import stats, broadcast
 
-api_id = 2332973
-api_hash = "157025425ad2637898b4710e019f809f"
-bot_token = "5768890343:AAFn6Ba_zpyC8wwhZhio4_qM-IuXIe1Infk"
+api_id = 
+api_hash = ""
+bot_token = ""
 
 app = Client(
     "bot",
