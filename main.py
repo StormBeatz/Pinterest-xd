d1df24c574783691bec4b7bd97d9224555386ef9
٭Bιĸαѕн٭ <127423650+Bikash1225@users.noreply.github.com>
2023-06-21 17:16:43 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from pyrogram import Client
+from ToXic.api import start, downloader, helper
+from ToXic.api.users import is_served_user, get_served_users, add_served_user
+from ToXic.api.broadcast import stats, broadcast
 
-api_id = 
-api_hash = ""
-bot_token = ""
+api_id = 2332973
+api_hash = "157025425ad2637898b4710e019f809f"
+bot_token = "5768890343:AAFn6Ba_zpyC8wwhZhio4_qM-IuXIe1Infk"
 
 app = Client(
     "bot",
     api_id=api_id,
     api_hash=api_hash,
     bot_token=bot_token,
-    plugins={"root": "ToXic.api"},
+    plugins={"root": "ToXic.api", "users": "ToXic.users", "broadcast": "ToXic.broadcast"},
 )
 
 app.run()
