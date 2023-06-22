90079c010b2ee9176551b2acd551745ce946f1fb
˹ᴧɴσɴʏᴍσᴜs ꭙ˼ <TheAnonymousX1025@gmail.com>
2023-06-22 09:36:55 +0530

    Update start.py

--- ToXic/api/start.py
+++ ToXic/api/start.py
@@ -1,8 +1,13 @@
-from ToXic.texts import start_msg
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from pyrogram.enums import ChatType
+
+from ToXic.texts import start_msg
+from ToXic.db import add_served_user
 
 
 @Client.on_message(filters.command("start"))
-async def start(_, msg: Message) -> Message:
-    return await msg.reply(start_msg)
+async def start(_, msg: Message):
+    if msg.chat.type == ChatType.PRIVATE:
+        await add_served_user(msg.from_user.id)
+    return await msg.reply_text(start_msg)
